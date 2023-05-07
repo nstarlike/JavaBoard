c18870d17f11b5752174c41359bf53a47abdcbf4
nstarlike <nstar0512@gmail.com>
2023-05-08 02:03:52 +0900

    Add User DAO class

---
+++ src/main/java/nstarlike/jcw/dao/UserDao.java
@@ -0,0 +1,15 @@
+package nstarlike.jcw.dao;
+
+import java.util.List;
+
+import nstarlike.jcw.model.User;
+
+public interface UserDao {
+	public List<User> readAll();
+	public User readByUserId(String userId);
+	public User readByLoginId(String loginId);
+	public User readByEmail(String email);
+	public int create(User user);
+	public int update(User user);
+	public int delete(Long userId);
+}
