d838cca1185df89112b00a7a1440cb8a611a07b8
nstarlike <nstar0512@gmail.com>
2023-05-08 14:36:36 +0900

    Add a service layer for User

---
+++ src/main/java/nstarlike/jcw/service/UserService.java
@@ -0,0 +1,15 @@
+package nstarlike.jcw.service;
+
+import java.util.List;
+
+import nstarlike.jcw.model.User;
+
+public interface UserService {
+	public List<User> getAll();
+	public User getByUserId(Long userId);
+	public User getByLoginId(String loginId);
+	public User getByEmail(String email);
+	public int create(User user);
+	public int update(User user);
+	public int delete(Long userId);
+}
