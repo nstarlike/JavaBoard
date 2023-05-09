3a76d6f41598008973315d9734a03d76a8b77336
nstarlike <nstar0512@gmail.com>
2023-05-10 00:12:58 +0900

    Rename userId to id

--- src/main/java/nstarlike/jcw/dao/UserDao.java
+++ src/main/java/nstarlike/jcw/dao/UserDao.java
@@ -1,11 +1,12 @@
 package nstarlike.jcw.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import nstarlike.jcw.model.User;
 
 public interface UserDao {
-	public List<User> readAll();
+	public List<User> readAll(Map<String, String> params);
 	public User readById(Long id);
 	public User readByLoginId(String loginId);
 	public User readByEmail(String email);
