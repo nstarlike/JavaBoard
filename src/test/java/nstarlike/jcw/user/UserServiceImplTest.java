360b9fb1398fea1913d8a59167af477a6d2dbe1a
nstarlike <nstar0512@gmail.com>
2023-05-10 22:19:52 +0900

    Create UserServiceImpl JUnit test skeleton

---
+++ src/test/java/nstarlike/jcw/user/UserServiceImplTest.java
@@ -0,0 +1,44 @@
+package nstarlike.jcw.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceImplTest {
+
+	@Test
+	void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetByLoginId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetByEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
