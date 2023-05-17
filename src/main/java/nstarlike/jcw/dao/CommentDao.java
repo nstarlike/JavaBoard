d3aff61623e8e103c533d49cede954eee8c1228a
nstarlike <nstar0512@gmail.com>
2023-05-17 20:43:52 +0900

    Modify CommentRepository for CommentMap

--- src/main/java/nstarlike/jcw/dao/CommentDao.java
+++ src/main/java/nstarlike/jcw/dao/CommentDao.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.Map;
 
 import nstarlike.jcw.model.Comment;
+import nstarlike.jcw.model.CommentMap;
 
 public interface CommentDao {
 	public int create(Comment comment);
-	public List<Comment> readAll(Map<String, String> params);
+	public List<CommentMap> readAll(Map<String, String> params);
 	public Comment readById(long id);
 	public int delete(long id);
 }
