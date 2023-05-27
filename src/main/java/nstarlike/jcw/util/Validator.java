1d604cf7d09cd9be3470ab39dc181a19fcd83400
nstarlike <nstar0512@gmail.com>
2023-05-28 04:56:39 +0900

    Modify message and return type
    Modify the return type of all methods from void to String.
    Modify messages in English.

--- src/main/java/nstarlike/jcw/util/Validator.java
+++ src/main/java/nstarlike/jcw/util/Validator.java
@@ -1,8 +1,9 @@
 package nstarlike.jcw.util;
 
 public class Validator {
-	private static final String ID_REGEX = "^[a-zA-Z]+[0-9a-zA-Z]{7,}$";
+	private static final String LOGIN_ID_REGEX = "^[a-zA-Z]+[0-9a-zA-Z]{7,}$";
 	private static final String PASSWORD_REGEX = "(?i).{6,20}";
+	private static final String KOREAN_NAME_REGEX = "^[가-힣0-9]+$";
 	private static final String CELL1_REGEX = "^01[016789]$";
 	private static final String CELL2_REGEX = "^[0-9]{3,4}$";
 	private static final String CELL3_REGEX = "^[0-9]{4}$";
@@ -19,137 +20,147 @@ public class Validator {
 	private static final String NUMBER_REGEX = "^[0-9]+$";
 	private static final String CHINESE_CHARACTER_REGEX = "^[一-龥]+$";
 	
-	public static void regex(String value, String regex, String msg) throws ValidatorInvalidException {
+	public static String regex(String value, String regex, String msg) throws ValidatorInvalidException {
 		if(!value.matches(regex)) {
 			throw new ValidatorInvalidException(msg);
 		}
+		
+		return value;
 	}
 	
-	public static void regex(String value, String regex) throws ValidatorInvalidException {
-		regex(value, regex, "형식이 올바르지 않습니다.");
+	public static String regex(String value, String regex) throws ValidatorInvalidException {
+		return regex(value, regex, "The format of value is invalid.");
 	}
 	
-	public static void id(String value, String msg) throws ValidatorInvalidException {
-		regex(value, ID_REGEX, msg);
+	public static String loginId(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, LOGIN_ID_REGEX, msg);
 	}
 	
-	public static void id(String value) throws ValidatorInvalidException {
-		id(value, "아이디 형식이 올바르지 않습니다.");
+	public static String loginId(String value) throws ValidatorInvalidException {
+		return loginId(value, "The format of your id is invalid.");
 	}
 	
-	public static void password(String value, String msg) throws ValidatorInvalidException {
-		regex(value, PASSWORD_REGEX, msg);
+	public static String password(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, PASSWORD_REGEX, msg);
 	}
 	
-	public static void password(String value) throws ValidatorInvalidException {
-		password(value, "비밀번호 형식이 올바르지 않습니다.");
+	public static String password(String value) throws ValidatorInvalidException {
+		return password(value, "The format of your password is invalid.");
 	}
 	
-	public static void cell1(String value, String msg) throws ValidatorInvalidException {
-		regex(value, CELL1_REGEX, msg);
+	public static String koreanName(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, KOREAN_NAME_REGEX, msg);
 	}
 	
-	public static void cell1(String value) throws ValidatorInvalidException {
-		cell1(value, "휴대폰 통신망번호 형식이 올바르지 않습니다.");
+	public static String koreanName(String value) throws ValidatorInvalidException {
+		return koreanName(value, "The format of your name is invalid.");
 	}
 	
-	public static void cell2(String value, String msg) throws ValidatorInvalidException {
-		regex(value, CELL2_REGEX, msg);
+	public static String cell1(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, CELL1_REGEX, msg);
 	}
 	
-	public static void cell2(String value) throws ValidatorInvalidException {
-		cell2(value, "휴대폰 지역번호 형식이 올바르지 않습니다.");
+	public static String cell1(String value) throws ValidatorInvalidException {
+		return cell1(value, "The format of area code of your cell number is invalid.");
 	}
 	
-	public static void cell3(String value, String msg) throws ValidatorInvalidException {
-		regex(value, CELL3_REGEX, msg);
+	public static String cell2(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, CELL2_REGEX, msg);
 	}
 	
-	public static void cell3(String value) throws ValidatorInvalidException {
-		cell3(value, "휴대폰 가입자번호 형식이 올바르지 않습니다.");
+	public static String cell2(String value) throws ValidatorInvalidException {
+		return cell2(value, "The format of exchange code of your cell number is invalid.");
 	}
 	
-	public static void cell(String value, String msg) throws ValidatorInvalidException {
-		regex(value, CELL_REGEX, msg);
+	public static String cell3(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, CELL3_REGEX, msg);
 	}
 	
-	public static void cell(String value) throws ValidatorInvalidException {
-		cell(value, "휴대폰 형식이 올바르지 않습니다.");
+	public static String cell3(String value) throws ValidatorInvalidException {
+		return cell3(value, "The format of carrier code of your cell number is invalid.");
 	}
 	
-	public static void tel1(String value, String msg) throws ValidatorInvalidException {
-		regex(value, TEL1_REGEX, msg);
+	public static String cell(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, CELL_REGEX, msg);
 	}
 	
-	public static void tel1(String value) throws ValidatorInvalidException {
-		tel1(value, "전화번호 국번 형식이 올바르지 않습니다.");
+	public static String cell(String value) throws ValidatorInvalidException {
+		return cell(value, "The format of your cell number is invalid.");
 	}
 	
-	public static void tel2(String value, String msg) throws ValidatorInvalidException {
-		regex(value, TEL2_REGEX, msg);
+	public static String tel1(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, TEL1_REGEX, msg);
 	}
 	
-	public static void tel2(String value) throws ValidatorInvalidException {
-		tel2(value, "전화번호 지역번호 형식이 올바르지 않습니다.");
+	public static String tel1(String value) throws ValidatorInvalidException {
+		return tel1(value, "The format of area code of your tel number is invalid.");
 	}
 	
-	public static void tel3(String value, String msg) throws ValidatorInvalidException {
-		regex(value, TEL3_REGEX, msg);
+	public static String tel2(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, TEL2_REGEX, msg);
 	}
 	
-	public static void tel3(String value) throws ValidatorInvalidException {
-		tel3(value, "전화번호 가입자번호 형식이 올바르지 않습니다.");
+	public static String tel2(String value) throws ValidatorInvalidException {
+		return tel2(value, "The format of exchange code of your tel number is invalid.");
 	}
 	
-	public static void tel(String value, String msg) throws ValidatorInvalidException {
-		regex(value, TEL_REGEX, msg);
+	public static String tel3(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, TEL3_REGEX, msg);
 	}
 	
-	public static void tel(String value) throws ValidatorInvalidException {
-		tel(value, "전화번호 형식이 올바르지 않습니다.");
+	public static String tel3(String value) throws ValidatorInvalidException {
+		return tel3(value, "The format of carrier code of your tel number is invalid.");
 	}
 	
-	public static void email1(String value, String msg) throws ValidatorInvalidException {
-		regex(value, EMAIL1_REGEX, msg);
+	public static String tel(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, TEL_REGEX, msg);
 	}
 	
-	public static void email1(String value) throws ValidatorInvalidException {
-		email1(value, "이메일 아이디 형식이 올바르지 않습니다.");
+	public static String tel(String value) throws ValidatorInvalidException {
+		return tel(value, "The format of your tel number is invalid.");
 	}
 	
-	public static void email2(String value, String msg) throws ValidatorInvalidException {
-		regex(value, EMAIL2_REGEX, msg);
+	public static String email1(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, EMAIL1_REGEX, msg);
 	}
 	
-	public static void email2(String value) throws ValidatorInvalidException {
-		email2(value, "이메일 도메인 형식이 올바르지 않습니다.");
+	public static String email1(String value) throws ValidatorInvalidException {
+		return email1(value, "The format of local part of your email is invalid.");
 	}
 	
-	public static void email(String value, String msg) throws ValidatorInvalidException {
-		regex(value, EMAIL_REGEX, msg);
+	public static String email2(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, EMAIL2_REGEX, msg);
 	}
 	
-	public static void email(String value) throws ValidatorInvalidException {
-		email(value, "이메일 형식이 올바르지 않습니다.");
+	public static String email2(String value) throws ValidatorInvalidException {
+		return email2(value, "The format of domain of your email is invalid.");
 	}
 	
-	public static void zipcode(String value, String msg) throws ValidatorInvalidException {
-		regex(value, ZIPCODE_REGEX, msg);
+	public static String email(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, EMAIL_REGEX, msg);
 	}
 	
-	public static void zipcode(String value) throws ValidatorInvalidException {
-		zipcode(value, "우편번호 형식이 올바르지 않습니다.");
+	public static String email(String value) throws ValidatorInvalidException {
+		return email(value, "The format of your email is invalid.");
 	}
 	
-	public static void korean(String value, String msg) throws ValidatorInvalidException {
-		regex(value, KOREAN_REGEX, msg);
+	public static String zipcode(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, ZIPCODE_REGEX, msg);
 	}
 	
-	public static void number(String value, String msg) throws ValidatorInvalidException {
-		regex(value, NUMBER_REGEX, msg);
+	public static String zipcode(String value) throws ValidatorInvalidException {
+		return zipcode(value, "The format of your zipcode is invalid.");
 	}
 	
-	public static void chinese(String value, String msg) throws ValidatorInvalidException {
-		regex(value, CHINESE_CHARACTER_REGEX, msg);
+	public static String korean(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, KOREAN_REGEX, msg);
+	}
+	
+	public static String number(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, NUMBER_REGEX, msg);
+	}
+	
+	public static String chineseCharacter(String value, String msg) throws ValidatorInvalidException {
+		return regex(value, CHINESE_CHARACTER_REGEX, msg);
 	}
 }
